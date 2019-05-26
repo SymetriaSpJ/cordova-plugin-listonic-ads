bbaae0fbeee5fa704b7d19f9263e7b2112c87fc4
Grzegorz <gczudowski@gmail.com>
2019-05-26 23:00:48 +0200

    wip

--- src/android/ListonicAds.java
+++ src/android/ListonicAds.java
@@ -96,12 +96,12 @@ public class ListonicAds extends CordovaPlugin {
                             1.0F
                         )
                     );
-                    ((ConstraintLayout) parentView).setOrientation(LinearLayout.VERTICAL);
+//                    ((ConstraintLayout) parentView).setOrientation(LinearLayout.VERTICAL);
                     parentView.setLayoutParams(
                         new ConstraintLayout.LayoutParams(
                             ConstraintLayout.LayoutParams.MATCH_PARENT,
-                            ConstraintLayout.LayoutParams.MATCH_PARENT,
-                            0.0F
+                            ConstraintLayout.LayoutParams.MATCH_PARENT
+//                            0.0F
                         )
                     );
 
