30e5b972d1d41eba7eb068617cb1c84e40652428
Grzegorz <gczudowski@gmail.com>
2019-05-26 20:15:10 +0200

    wip

--- src/android/ListonicAds.java
+++ src/android/ListonicAds.java
@@ -121,12 +121,6 @@ public class ListonicAds extends CordovaPlugin {
 
                 listonicAd = new DisplayAdContainer(webView.getContext());
                 listonicAd.setBackgroundColor(Color.parseColor("#F7F8F9"));
-//                listonicAd.setLayoutParams(
-//                        new LinearLayout.LayoutParams(
-//                                ViewGroup.LayoutParams.MATCH_PARENT,
-//                                ViewGroup.LayoutParams.WRAP_CONTENT
-//                        )
-//                );
                 listonicAd.setLayoutParams(
                         new LinearLayout.LayoutParams(
                                 ViewGroup.LayoutParams.MATCH_PARENT,
