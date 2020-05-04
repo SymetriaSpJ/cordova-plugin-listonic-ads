04aa84fe174870fe115954d91fbe6c11fcbc4b14
grzegorz <grzegorz.czudowski@fitatu.com>
2020-05-04 07:35:34 +0200

    wip

--- src/android/ListonicAds.java
+++ src/android/ListonicAds.java
@@ -143,6 +143,7 @@ public class ListonicAds extends CordovaPlugin {
                     rootView.addView(parentView);
                 }
 
+                parentView.setGravity(Gravity.TOP)
                 parentView.bringToFront();
                 parentView.requestLayout();
                 parentView.requestFocus();
