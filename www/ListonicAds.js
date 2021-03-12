cc2b0308801499c0b5fd1a466cd8b9e077fc09b0
gczudowski <gczudowski@users.noreply.github.com>
2021-03-12 08:53:41 +0100

    Fix error callbacks

--- www/ListonicAds.js
+++ www/ListonicAds.js
@@ -2,61 +2,51 @@ var exec = cordova.require('cordova/exec');
 
 var ListonicAds = function() {};
 
-ListonicAds.prototype.initBanner = function(onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
-    var successCallback = function(obj) {
-        onSuccess(obj);
-    };
+ListonicAds.prototype.initBanner = function() {
+    var successCallback = function() {};
+    var errorCallback = function() {};
 
     exec(successCallback, errorCallback, 'ListonicAds', 'initBanner', []);
 };
 
-ListonicAds.prototype.initInterstitial = function(onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
-    var successCallback = function(obj) {
-        onSuccess(obj);
-    };
+ListonicAds.prototype.initInterstitial = function() {
+    var successCallback = function() {};
+    var errorCallback = function() {};
 
     exec(successCallback, errorCallback, 'ListonicAds', 'initInterstitial', []);
 };
 
 ListonicAds.prototype.show = function(options, onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
     var successCallback = function(obj) {
         onSuccess(obj);
+    };
+
+    var errorCallback = function(obj) {
+        onError(obj);
     };
 
     exec(successCallback, errorCallback, 'ListonicAds', 'show', [options]);
 };
 
 ListonicAds.prototype.showInterstitial = function(options, onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
     var successCallback = function(obj) {
         onSuccess(obj);
+    };
+
+    var errorCallback = function(obj) {
+        onError(obj);
     };
 
     exec(successCallback, errorCallback, 'ListonicAds', 'showInterstitial', [options]);
 };
 
 ListonicAds.prototype.setOptions = function(options, onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
     var successCallback = function(obj) {
         onSuccess(obj);
+    };
+
+    var errorCallback = function(obj) {
+        onError(obj);
     };
 
     if (!options.width) {
@@ -67,43 +57,43 @@ ListonicAds.prototype.setOptions = function(options, onSuccess, onError) {
 };
 
 ListonicAds.prototype.hide = function(onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
     var successCallback = function(obj) {
         onSuccess(obj);
+    };
+
+    var errorCallback = function(obj) {
+        onError(obj);
     };
 
     exec(successCallback, errorCallback, 'ListonicAds', 'hide', []);
 };
 
 ListonicAds.prototype.setDebugMode = function(options) {
-    var errorCallback = function() {};
     var successCallback = function() {};
+    var errorCallback = function() {};
 
     exec(successCallback, errorCallback, 'ListonicAds', 'setDebugMode', [options]);
 };
 
 ListonicAds.prototype.hasConsent = function(onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
     var successCallback = function(obj) {
         onSuccess(obj);
+    };
+
+    var errorCallback = function(obj) {
+        onError(obj);
     };
 
     exec(successCallback, errorCallback, 'ListonicAds', 'hasConsent', []);
 };
 
 ListonicAds.prototype.updateGdprConsentsData = function(options, onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
     var successCallback = function(obj) {
         onSuccess(obj);
+    };
+
+    var errorCallback = function(obj) {
+        onError(obj);
     };
 
     exec(successCallback, errorCallback, 'ListonicAds', 'updateGdprConsentsData', [options]);
